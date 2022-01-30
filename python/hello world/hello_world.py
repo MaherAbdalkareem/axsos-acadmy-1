fb72ed800e5a9a8f999d045e8df8285852a3fdbc
maher abdalkareem <maherbaqa53@gmail.com>
2022-01-30 20:16:08 +0200

    commit

---
+++ python/hello world/hello_world.py
@@ -0,0 +1,14 @@
+str="hello world"
+print(str)
+name="maher"
+print("hello",name)
+print("hello "+name)
+num="6"
+print("hello",num)
+print("hello "+num)
+food1="mansaf"
+food="shawerma"
+print("i love to eat {} and {}".format(food1,food))
+print(f"i love to eat {food1} and {food}")
+print(str.split("l"))
+print(str.count("l"))
