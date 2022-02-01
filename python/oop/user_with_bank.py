6653b31d9dca4b995128126900d3f29fc45d81e7
maher abdalkareem <maherbaqa53@gmail.com>
2022-02-01 16:13:15 +0200

    commit

--- python/oop/user_with_bank.py
+++ python/oop/user_with_bank.py
@@ -1,47 +1,52 @@
 class bankAccount:
-    def __init__(self,int_rate=0.01,balance=0,name="acc"):
+    def __init__(self,int_rate=0.01,balance=0):
         self.int_rate=int_rate
         self.balance=balance
-        self.name=name
-    def deposit(self, amount,name):
-        name.balance+=amount
+    def deposit(self, amount):
+        self.balance+=amount
         return self
-    def withdraw (self,amount,name):
-        if name.balance<amount :
+    def withdraw (self,amount):
+        if self.balance<amount :
             print("Insufficient funds: Charging a $5 fee and deduct $5")
         else:
-            name.balance-=amount
+            self.balance-=amount
         return self
-    def display_account_info(self,name):
-        print("Balance : ",name.balance)
+    def display_account_info(self):
+        print("Balance : " ,self.balance)
         return self
-    def yield_interest(self,name):
-        name.balance=(name.balance*name.int_rate)+name.balance
+    def yield_interest(self):
+        self.balance=(self.balance*self.int_rate)+self.balance
         return self
+
 
 class user:
     def __init__(self,name,age,address):
         self.name=name
         self.age=age
         self.address=address
-        self.account=bankAccount(int_rate=0.02,balance=0,name="acc")
-    def make_deposit(self,amount,name):
-        name.account.deposit(amount)
+        self.account=[bankAccount(int_rate=0.02,balance=0)]
+        
+    def make_deposit(self,amount,index):
+        self.account.append(bankAccount(int_rate=0.02,balance=0))
+        self.account[index].balance +=amount
+        
         return self
-    def make_withdrawal(self,amount,name):
-        name.account.withdraw(amount)
+    def make_withdrawal(self,amount,index):
+        self.account.append(bankAccount(int_rate=0.02,balance=0))
+        self.account[index].balance-=amount
         return self
-    def display_user_balance(self,name):
-        name.account.display_account_info()
+    def display_user_balance(self,index):
+        print(self.account[index].balance)
         return self
 
 
+acc1=bankAccount(0.02,0) #index 0
+acc2=bankAccount(0.02,0) #index 1
+acc3=bankAccount(0.02,0)
+acc4=bankAccount(0.02,0)
 
-
-
-account1=bankAccount(0.03,0,"account1")
-account2=bankAccount(0.04,0,"account2")
-account3=bankAccount(0.02,0,"account3")
 
 maher=user("maher",24,"baqa")
-maher.account.deposit(100,account1).deposit(200,account2).display_account_info(account2)
+abed=user("abed",24,"baqa")
+maher.make_deposit(100,0).make_deposit(200,2).make_withdrawal(100,2).make_deposit(200,3).display_user_balance(0).display_user_balance(2).display_user_balance(3)
+
