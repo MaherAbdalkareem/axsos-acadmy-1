2aae3d01b2b2e60b2c708fcf1c11e73bcd946b89
maher abdalkareem <maherbaqa53@gmail.com>
2022-04-16 19:08:46 +0300

    commit

---
+++ MERN/React/pokemon_api/src/component/Form.js
@@ -0,0 +1,25 @@
+import React ,{useState , useEffect} from 'react'
+
+const Form = (props) => {
+    const [pokemons, setPokemons] = useState([]);
+
+    const handler = ()=>{
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        .then(response => response.json())
+        .then(response => setPokemons(response.results))
+    }
+    return (
+        <div>
+            <div>
+            <button onClick={handler}>Fetch Pokemons</button>
+            </div>
+            <div>
+                {pokemons.map((pokemon, index)=>{
+                    return (<div key={index}>{pokemon.name}</div>)
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Form
