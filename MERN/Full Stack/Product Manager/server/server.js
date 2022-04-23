e933ed40751e216ea4a7ad69c52a5df9e5f1c0e6
maher abdalkareem <maherbaqa53@gmail.com>
2022-04-24 01:29:12 +0300

    commit

---
+++ MERN/Full Stack/Product Manager/server/server.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const cors = require('cors')
+const app = express();
+require('./config/ProductConfig')
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+require('./routes/ProductRoutes')(app);
+app.listen(8000, () => console.log(`listen in port : 8000`))
