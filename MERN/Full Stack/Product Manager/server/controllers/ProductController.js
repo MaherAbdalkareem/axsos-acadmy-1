d63bdc39d5cb3f3bf4f0bbe94e2cce3362259c8d
maher abdalkareem <maherbaqa53@gmail.com>
2022-04-25 01:26:19 +0300

    commit

--- MERN/Full Stack/Product Manager/server/controllers/ProductController.js
+++ MERN/Full Stack/Product Manager/server/controllers/ProductController.js
@@ -1,4 +1,5 @@
 
+const { request, response } = require('express');
 const { Product } = require('../models/ProductModel');
 
 module.exports.createProduct = (request,response) => {
@@ -22,5 +23,17 @@ module.exports.getProduct = (request,response) => {
     const { id } = request.params;
     Product.findOne({_id:id})
     .then(product => response.json(product))
+    .catch(err => response.json(err))
+}
+module.exports.updateProduct = (request , response) => {
+    const {id} = request.params ;
+    Product.findOneAndUpdate({_id:id}, request.body, {new:true})
+    .then(product => response.json(product))
+    .catch(err => response.json(err))
+}
+module.exports.deleteProduct = (request,response) => {
+    const {id} =request.params ;
+    Product.deleteOne({_id:id})
+    .then(deletedProduct => response.json(deletedProduct))
     .catch(err => response.json(err))
 }
--- MERN/Full Stack/Product Manager/server/routes/ProductRoutes.js
+++ MERN/Full Stack/Product Manager/server/routes/ProductRoutes.js
@@ -2,6 +2,8 @@ const ProductController = require('../controllers/ProductController');
 
 module.exports = function (app){
     app.post('/api/product/new', ProductController.createProduct);
-    app.get('/api/product/new',ProductController.getAllProduct)
-    app.get('/api/product/:id',ProductController.getProduct)
+    app.get('/api/product/new',ProductController.getAllProduct);
+    app.get('/api/product/:id',ProductController.getProduct);
+    app.put('/api/product/:id/edit',ProductController.updateProduct)
+    app.delete('/api/product/delete/:id',ProductController.deleteProduct)
 }
