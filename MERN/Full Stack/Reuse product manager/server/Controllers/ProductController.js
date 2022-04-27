d650e14283483a9765f7b6058af5ab27bb21aaf7
maher abdalkareem <maherbaqa53@gmail.com>
2022-04-27 14:27:32 +0300

    commit

---
+++ MERN/Full Stack/Reuse product manager/server/Controllers/ProductController.js
@@ -0,0 +1,38 @@
+const { request, response } = require('express');
+const {Product} = require('../Models/ProductModel');
+
+module.exports.createProduct = (request,response) => {
+    const { title , price , description } = request.body ;
+    Product.create({
+        title,
+        price,
+        description
+    })
+    .then(product => response.json(product))
+    .catch(err => response.json(err))
+}
+
+module.exports.getAllProduct = (request,response) => {
+    Product.find({})
+    .then(products => response.json(products))
+    .catch(err => response.json(err))
+}
+
+module.exports.getProduct = (request,response) => {
+    const { id } = request.params;
+    Product.findOne({_id:id})
+    .then(product => response.json(product))
+    .catch(err => response.json(err))
+}
+module.exports.updateProduct = (request , response) => {
+    const {id} = request.params ;
+    Product.findOneAndUpdate({_id:id}, request.body, {new:true})
+    .then(product => response.json(product))
+    .catch(err => response.json(err))
+}
+module.exports.deleteProduct = (request,response) => {
+    const {id} =request.params ;
+    Product.deleteOne({_id:id})
+    .then(deletedProduct => response.json(deletedProduct))
+    .catch(err => response.json(err))
+}
---
+++ MERN/Full Stack/Reuse product manager/server/routes/ProductRoutes.js
@@ -0,0 +1,8 @@
+const ProductController = require('../Controllers/ProductController')
+module.exports = function (app){
+    app.post('/api/product/new', ProductController.createProduct);
+    app.get('/api/product/new',ProductController.getAllProduct);
+    app.get('/api/product/:id',ProductController.getProduct);
+    app.put('/api/product/:id/edit',ProductController.updateProduct)
+    app.delete('/api/product/delete/:id',ProductController.deleteProduct)
+}
